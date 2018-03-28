58110c982c64610e82b76827eb239bc3ea7d7654
Pawel Struminski <pstruminski@gmail.com>
2018-03-28 13:47:27 +0200

    header improvments

--- wp-content/themes/mks/templates/header.php
+++ wp-content/themes/mks/templates/header.php
@@ -72,14 +72,30 @@
               <p><?php is_front_page() ? bloginfo('description') : wp_title("",true);  ?></p>
           </div>
       </div>
-      <div class="row">
-          <div class="col-sm motto">
-              <div class="puchar"></div>
-              <p>Naszą misją jest wspieranie młodych sportowców <br>
-               ich drodze do sukcesu, życia w duchu fair play<br>
-               oraz odpowiedzialności za siebie i innych.</p>
-              <div class="timer"></div>
-          </div>
+    <div class="row">
+    <?php if (is_page('gadzety')): ?>
+
+        <div class='col-sm motto'>
+            <div class='puchar'></div>
+            <p>Twója stara</p>
+            <div class='timer'></div>
+        </div>;
+
+    <?php else: ?>
+
+        <div class='col-sm motto'>
+            <div class='puchar'></div>
+            <p>Naszą misją jest wspieranie młodych sportowców <br>
+                ich drodze do sukcesu, życia w duchu fair play<br>
+                oraz odpowiedzialności za siebie i innych.</p>
+            <div class='timer'></div>
+        </div>;
+
+    <?php endif ?>
+
+
+
+
       </div>
 
 </header>
